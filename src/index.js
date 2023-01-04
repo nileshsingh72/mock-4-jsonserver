a5046dea2c9ab60cedeca43e914743cb7ffb1538
nileshsingh72 <rnileshchauhan89@gmail.com>
2023-01-04 12:13:57 +0530

    json-server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const js = require("json-server");
 const server = js.create();
 
-const router = js.router("db.json");
+const router = js.router("../db.json");
 const middlware = js.defaults();
 const port = 8800;
 
